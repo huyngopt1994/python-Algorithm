09e614073cb7c55ce0b055b47650f4295107c7a2
Huy Ngo <huyngopt1994@gmail.com>
2018-06-18 23:55:16 +0700

    remove space in a string

---
+++ green/green-07-string/remove_space_in_a_string.py
@@ -0,0 +1,23 @@
+"""
+Input: '    bala      sasa      dsds    dsds   '
+output : 'bala sasa dsds dsds'
+"""
+n = input()
+
+def remove_space(my_string):
+    # because string is immutable so we have to create a new string and copy it
+    res= ''
+    need_a_space = False
+    for c in my_string:
+        if c !=' ':
+            if need_a_space:
+                res += ' '
+                need_a_space = False
+            res += c
+        elif len(res)>0 and res[-1] != ' ':
+            need_a_space = True
+    return res
+for _ in range(int(n)):
+    my_string = input()
+    res = remove_space(my_string)
+    print(res)
