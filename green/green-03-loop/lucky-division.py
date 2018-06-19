8b5776e3c99bf064b053fbd99a0bde7350f8428f
Huy Ngo <huyngopt1994@gmail.com>
2018-06-19 12:12:27 +0700

    complete lucky number in codeforce

---
+++ green/green-03-loop/lucky-division.py
@@ -0,0 +1,24 @@
+"http://codeforces.com/problemset/problem/122/A"
+#technic flag
+
+flag = True
+s = input()
+
+def verify_contain_lucky_number(s):
+    flag = True
+    for c in s:
+        if c not in ['4', '7']:
+            flag = False
+            break
+    return flag
+flag =False
+for i in range(1,1001):
+    if verify_contain_lucky_number(str(i)):
+        if int(s) % i == 0 :
+            flag = True
+            break
+
+if flag:
+    print('YES')
+else:
+    print('NO')
