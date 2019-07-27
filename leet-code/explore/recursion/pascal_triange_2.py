f1131e4edd3b5b0c69f13afa387fe06273b80711
vuhuy@inspectorio.com <vuhuy@inspectorio.com>
2019-07-27 12:38:39 +0700

    Add pascal triange 2 solution

--- leet-code/explore/recursion/pascal_triange_2.py
+++ leet-code/explore/recursion/pascal_triange_2.py
@@ -4,14 +4,12 @@ hash_table = {}
 
 
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
+    def getRow(self, rowIndex: int) -> List[int]:
         result = []
-        for i in range(1, numRows + 1):
-            result.append([])
-            for j in range(1, i + 1):
-                point = self.generate_per_point(i, j)
-                hash_table[(i, j)] = point
-                result[i - 1].append(point)
+        for j in range(1, rowIndex + 2):
+            point = self.generate_per_point(rowIndex + 1, j)
+            hash_table[(rowIndex + 1, j)] = point
+            result.append(point)
         return result
 
     def generate_per_point(self, i, j):
